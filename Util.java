01aaf6eaa4dcfb24e69f38837bc285279f30bc6f
pengfugen <pfg0717@163.com>
2017-12-12 16:29:51 +0800

    update
    	modified:   README.md
    	new file:   Util.java

---
+++ Util.java
@@ -0,0 +1,16 @@
+
+/**
+ * Created by pengfugen on 16-1-11.
+ */
+
+package com.wind.pfg.mephone.util;
+
+import android.util.Log;
+
+public class Util {
+
+    public static void logd(String tag,String message)
+    {
+        Log.d(tag,message);
+    }
+}
